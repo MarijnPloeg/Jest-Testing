377b3583e9d63f46701d059595daafb642dd1e9c
Marijn Ploeg <marijnploeg@gmail.com>
2021-01-27 15:44:20 +0100

    Implement findByName() + test

--- index.test.js
+++ index.test.js
@@ -1,6 +1,14 @@
 function add(num1, num2) {
-    const sum = num1 + num2;
-    return sum;
+    return num1 + num2;
+}
+
+function findByName(users, name){
+    for (const user of users) {
+        if (user.name === name){
+            return user;
+        }
+    }
+    return null;
 }
 
 
@@ -18,3 +26,23 @@ test('add() function should return the sum of 2 numbers', () => {
     expect(sum).toBe(9); //Expect en toBe komen uit Jest framework
 });
 
+test('findByName() returns an object from an array with a specified name', () => {
+    //Arrange
+    const users = [{id: 1, name: 'Marijn'}, {id: 2, name: 'Michiel'}, {id: 3, name: 'Martijn'}]
+    const userNameToFind = 'Michiel';
+    //Act
+    const user = findByName(users, userNameToFind);
+    //Assert
+    expect(user).toEqual({id:2, name: 'Michiel'})
+});
+
+test('findByName() should return null if the element is not found', () => {
+    //Arrange
+    const users = [{id: 1, name: 'Marijn'}, {id: 2, name: 'Michiel'}, {id: 3, name: 'Martijn'}]
+    const userNameToFind = 'Kees';
+    //Act
+    const user = findByName(users, userNameToFind);
+    //Assert
+    expect(user).toBeNull();
+
+});
