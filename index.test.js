ec1b9968fba8b45a5d0804e798de52a334592da5
Marijn Ploeg <marijnploeg@gmail.com>
2021-01-27 12:23:20 +0100

    write test and implement function

--- index.test.js
+++ index.test.js
@@ -1,4 +1,20 @@
-test('Even kijken of dit werkt', () => {
+function add(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
+
+
+test('add() function should return the sum of 2 numbers', () => {
     console.log('Hallo, dit is een test')
+
+    //Arrange
+    const num1 = 4;
+    const num2 = 5;
+
+    //Act
+    const sum = add(num1, num2);
+
+    //Assert
+    expect(sum).toBe(9); //Expect en toBe komen uit Jest framework
 });
 
